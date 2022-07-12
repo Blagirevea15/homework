c3da04282743447f269f93f57208979078d3c25e
Blagirev <blagirevea@mail.ru>
2022-07-12 17:33:41 +0300

    Задача 6

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
-﻿Console.Write ("Введите число A: ");
-int numberA = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Введите число B: ");
-int numberB = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Введите число C: ");
-int numberC = Convert.ToInt32 (Console.ReadLine());
-if (numberA >= numberB && numberA >= numberC)
-{  
-    Console.WriteLine ($"{numberA } максимальное число.");
+﻿Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine ($"{number } четное число.");   
 }
-if (numberB >= numberA && numberB >= numberC)
-{  
-    Console.WriteLine ($"{numberB } максимальное число.");
-}
-if (numberC >= numberA && numberC >= numberB)
-{  
-    Console.WriteLine ($"{numberC } максимальное число.");
+else 
+{
+    Console.WriteLine ($"{number } нечетное число.");   
 }
