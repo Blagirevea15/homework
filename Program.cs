bddbbf4480d94932a9c5927f202dccac2b6fc31d
Blagirev <blagirevea@mail.ru>
2022-07-12 17:23:28 +0300

    Задача 4

--- Program.cs
+++ Program.cs
@@ -2,15 +2,17 @@
 int numberA = Convert.ToInt32 (Console.ReadLine());
 Console.Write ("Введите число B: ");
 int numberB = Convert.ToInt32 (Console.ReadLine());
-if (numberA > numberB)
-{
-    Console.WriteLine ($"{numberA } больше {numberB}");
+Console.Write ("Введите число C: ");
+int numberC = Convert.ToInt32 (Console.ReadLine());
+if (numberA >= numberB && numberA >= numberC)
+{  
+    Console.WriteLine ($"{numberA } максимальное число.");
 }
-if (numberA == numberB)
-{
-    Console.WriteLine ($"{numberA } равно {numberB}");
+if (numberB >= numberA && numberB >= numberC)
+{  
+    Console.WriteLine ($"{numberB } максимальное число.");
 }
-else
-{
-    Console.WriteLine ($"{numberA } меньше {numberB}");
+if (numberC >= numberA && numberC >= numberB)
+{  
+    Console.WriteLine ($"{numberC } максимальное число.");
 }
