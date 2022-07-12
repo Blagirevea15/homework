47c9adb7ab04e8e0ce0e021a59385916daec701d
Blagirev <blagirevea@mail.ru>
2022-07-12 15:34:08 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите число A: ");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите число B: ");
+int numberB = Convert.ToInt32 (Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine ($"{numberA } больше {numberB}");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine ($"{numberA } равно {numberB}");
+}
+else
+{
+    Console.WriteLine ($"{numberA } меньше {numberB}");
+}
